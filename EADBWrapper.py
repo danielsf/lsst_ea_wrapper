f7a7621ebb017415217ad4eaa4c23c4d977b17c8
danielsf <scott.f.daniel@gmail.com>
2015-10-16 14:30:13 -0700

    get daughter objects

--- EADBWrapper.py
+++ EADBWrapper.py
@@ -10,10 +10,31 @@ class EADBWrapper(object):
         self._dbo = DBObject(database='sysarch', host='terminal.lsst.org',
                              port='3306', driver='mysql')
 
+        self._objSearchDtype = np.dtype([('name', str, 100), ('ObjectID', np.int)])
+
 
     def objectIdFromName(self, name):
 
-        dtype = np.dtype([('name', str, 100), ('ObjectID', np.int)])
         query = "select t.name, t.Object_ID from t_object t where t.name = '%s'" % name
+        results = self._dbo.execute_arbitrary(query, dtype=self._objSearchDtype)
+        return results['ObjectID']
+
+
+    def daughterObjectsFromID(self, parentID):
+ 
+        dtype = np.dtype([('name',str,100), ('ObjectID',np.int)])
+        query = "select t.name, t.Object_ID from t_object t where t.ParentID = %d" % parentID
         results = self._dbo.execute_arbitrary(query, dtype=dtype)
-        return results['ObjectID']   
+        nameList = results['name']
+        objIdList = results['ObjectID']
+
+        propertyIdList = []
+        dtype = np.dtype([('PropertyID', np.int)])
+        for objId in objIdList:
+            query = "select t.PropertyID from t_objectproperties t where t.Object_ID = %d" % objId
+            results = self._dbo.execute_arbitrary(query, dtype=dtype)
+            if len(results)!=1:
+                 raise RuntimeError("Not sure what to do; got more than one property in daughterObjectsFromID")
+            propertyIdList.append(results['PropertyID'][0])
+
+        return nameList, objIdList, propertyIdList
--- testScripty.py
+++ testScripty.py
@@ -5,3 +5,7 @@ if __name__ == "__main__":
     dbo = EADBWrapper()
     objID = dbo.objectIdFromName('Secondary (M2) Adjustment')
     print objID
+    nameList, objList, propList = dbo.daughterObjectsFromID(objID[0])
+    print nameList
+    print objList
+    print propList
