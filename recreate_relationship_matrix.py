4c6aabd1380296efc54c761b162dbf9cd2ae561a
danielsf <scott.f.daniel@gmail.com>
2015-12-04 13:25:35 -0800

    hacking

--- recreate_relationship_matrix.py
+++ recreate_relationship_matrix.py
@@ -5,5 +5,8 @@ from EADBWrapper import EADBWrapper
 if __name__ == "__main__":
 
     eadbo = EADBWrapper()
+    property_dtype = np.dtype([('Object_ID', np.int),
+                               ('Property', str, 300),
+                               spock
 
 
