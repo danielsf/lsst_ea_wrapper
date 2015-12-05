bd027bbfc3dc9296971c8339e9b0020f189330b2
danielsf <scott.f.daniel@gmail.com>
2015-12-05 09:01:08 -0800

    recreate_relationship_matrix prints all LSSTRequirements from t_objectproperties

--- recreate_relationship_matrix.py
+++ recreate_relationship_matrix.py
@@ -5,8 +5,19 @@ from EADBWrapper import EADBWrapper
 if __name__ == "__main__":
 
     eadbo = EADBWrapper()
+    dbo = eadbo._dbo
     property_dtype = np.dtype([('Object_ID', np.int),
                                ('Property', str, 300),
-                               spock
+                               ('PropertyID', np.int),
+                               ('Value', str, 300),
+                               ('Notes', str, 300)])
+
+    property_query = "select t.Object_ID, t.Property, t.PropertyID, " \
+                      + "t.Value, t.Notes from t_objectproperties t " \
+                      + "where t.Property like '%lsstrequire%'"
+
+    results = dbo.execute_arbitrary(property_query, dtype=property_dtype)
+    print results
+    print len(results)
 
 
