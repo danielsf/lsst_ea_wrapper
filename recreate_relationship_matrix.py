598ea10616d113f18e6ed436ccaeb7beb6a88b31
danielsf <scott.f.daniel@gmail.com>
2015-12-05 09:52:33 -0800

    recreate_relationship_matrix lists all relationships between requirements

--- recreate_relationship_matrix.py
+++ recreate_relationship_matrix.py
@@ -6,20 +6,9 @@ if __name__ == "__main__":
 
     eadbo = EADBWrapper()
     dbo = eadbo._dbo
-    property_dtype = np.dtype([('Object_ID', np.int),
-                               ('Property', str, 300),
-                               ('PropertyID', np.int),
-                               ('Value', str, 300),
-                               ('Notes', str, 300)])
 
-    property_query = "select t.Object_ID, t.Property, t.PropertyID, " \
-                      + "t.Value, t.Notes from t_objectproperties t " \
-                      + "where t.Property like '%lsstrequire%'"
-
-    property_results = dbo.execute_arbitrary(property_query,
-                                             dtype=property_dtype)
-
-    property_id_list = property_results['Object_ID']
+    # Query t_connector for all of the connections between two properties
+    # labeled as 'LSSTRequirement'
 
     connection_dtype = np.dtype([('Btm_Mid_Label', str, 300),
                                 ('Start_Object_ID', np.int),
@@ -31,9 +20,39 @@ if __name__ == "__main__":
                        + "on p.Object_ID = c.Start_Object_ID " \
                        + "where p.Property like '%lsstrequire%'"
 
-    result = dbo.execute_arbitrary(connection_query,
-                                   dtype=connection_dtype)
+    connection_list = dbo.execute_arbitrary(connection_query,
+                                           dtype=connection_dtype)
 
-    print result
-    print len(result)
 
+    # Query t_object for all of the objects at either end of those
+    # connections
+
+    object_dtype = np.dtype([('Name', str, 300),
+                             ('Object_ID', np.int)])
+
+    object_query = "select distinct o.Name, o.Object_ID from t_object o " \
+                   + "inner join t_connector c " \
+                   + "on c.End_Object_ID=o.Object_ID " \
+                   + "or c.Start_Object_ID=o.Object_ID " \
+                   + "inner join t_objectproperties p " \
+                   + "on p.Object_ID = c.Start_Object_ID " \
+                   + "where p.Property like '%lsstrequire%'"
+
+    object_list = dbo.execute_arbitrary(object_query,
+                                        dtype=object_dtype)
+
+
+    with open('test_matrix.txt', 'w') as output_file:
+        for connection in connection_list:
+            cc = connection['Btm_Mid_Label'].replace('\r','')
+            cc = cc.replace('\n', '')
+            cc = cc.strip()
+            if cc != 'None':
+            
+                start_dex = np.where(object_list['Object_ID'] == connection['Start_Object_ID'])[0][0]
+                end_dex = np.where(object_list['Object_ID'] == connection['End_Object_ID'])[0][0]
+                start_name = object_list['Name'][start_dex].replace('\n','')
+                start_name = start_name.replace('\r', '')
+                end_name = object_list['Name'][end_dex].replace('\n','')
+                end_name = end_name.replace('\r', '')
+                output_file.write("%s %s %s\n" % (start_name, cc, end_name))
